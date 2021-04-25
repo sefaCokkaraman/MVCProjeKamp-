ac8c5f117f5d2a82f5381f275d3285616ffe271b
sefaCokkaraman <sefacokkaraman@hotmail.com>
2021-04-25 15:43:45 +0300

    MvcProjeKampı
    1.Görev

---
+++ MVCProjeKampı/BusinnesLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,38 @@
+﻿using DataAccesLayer.Concrete.Repositories;
+using EntityLayer.Concerete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinnesLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+        public List<Category> GetAllBL()
+        {
+
+            return repo.List();
+
+        }
+        public void CategoryAddBL(Category p)
+        {
+
+            if (p.CategoryName==null||p.CategoryName.Length<3||p.CategoryDescription==""||p.CategoryName.Length>=51)
+            {
+                //Hata Mesajı
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+
+
+
+        }
+
+
+    }
+}
---
+++ MVCProjeKampı/DataAccesLayer/Migrations/202104241750558_mig1.cs
@@ -0,0 +1,20 @@
+﻿namespace DataAccesLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+          
+
+            AlterColumn("dbo.Categories", "CategoryStatus", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Categories", "CategoryStatus", c => c.String());
+        }
+    }
+}
